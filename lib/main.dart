b179f1615f411631c4c4943ec5cc948884d76658
SaraK <sarach@appupgo.co.il>
2023-01-31 14:31:46 +0200

    add screen utils for designing responisive

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
-import 'package:eshkolot_offline/isar_service.dart';
+
+import 'package:eshkolot_offline/services/isar_service.dart';
 import 'package:eshkolot_offline/models/subject.dart';
+import 'package:eshkolot_offline/services/vimoe_service.dart';
 import 'package:eshkolot_offline/ui/screens/course_main/course_main_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'models/course.dart';
 import 'models/lesson.dart';
 import 'models/questionnaire.dart';
 import 'dart:convert';
-import 'package:dio/dio.dart';
-
-
 
 late Course? course;
 
 Future<void> main() async {
   IsarService.instance.init();
  await initData();
- await connectToVimoe();
+ // await VimoeService().connectToVimoe();
   runApp(MyApp());
 }
 
@@ -101,27 +101,8 @@ initData() async {
       print('data is filled');
 }
 
-connectToVimoe() async
- {
-   // String token='fb7f92745521f268deae101b7e3abbc4';
-   // String token='4eda357f555a3eef86e254b9bf012d85';//privet
-   String token='adf22872bed862a3aa5993a3133c5fa2';//privet video files
-   int videoId=789969851;
-   Dio dio =  Dio();
-   dio.options.headers['content-Type'] = 'application/json;charset=UTF-8';
-   dio.options.headers["authorization"] = "bearer ${token}";
-   // Response response = await dio.get('https://api.vimeo.com/apps/1bba3dc3af2a22c466bfcd4f15817a600b0284e7');
-   Response response = await dio.get('https://api.vimeo.com/videos/${videoId}');
 
-   Map result = response.data;
-   print('privacy ${result['privacy']}');
 
-   print("data ${response.data}");
-   print("headers ${response.headers}");
-   print("requestOptions ${response.requestOptions}");
-   print("statusCode ${response.statusCode}");
-;
- }
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
@@ -129,21 +110,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(1920, 1080),
+        minTextAdapt:true,
+      builder: (BuildContext context, Widget? child) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: FutureBuilder<Course?>(
-          future: getFirstEnglishCourse(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              course = snapshot.data;
-              return CourseMainPage(course: course!);
-            }
-            print('wait...');
-            return const CircularProgressIndicator();
-          }),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: FutureBuilder<Course?>(
+            future: getFirstEnglishCourse(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                course = snapshot.data;
+                return CourseMainPage(course: course!);
+              }
+              print('wait...');
+              return const CircularProgressIndicator();
+            }),
+      ); }
     );
   }
 
--- lib/services/isar_service.dart
+++ lib/services/isar_service.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 
-import 'models/course.dart';
-import 'models/subject.dart';
+import '../models/course.dart';
+import '../models/subject.dart';
 
 class IsarService {
   late Future<Isar> db;
